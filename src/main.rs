1ab13a14737ed32ee2ca5e9dad0909008fe5ec4f
Bytomancer <dennis.lawter@gmail.com>
2022-11-29 01:45:12 -0500

    Solved up to d3s1

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,10 @@ fn main() {
     match &func[..] {
         "d1s1" => solutions::day1::d1s1(),
         "d1s2" => solutions::day1::d1s2(),
-        _ => {},
+        "d2s1" => solutions::day2::d2s1(),
+        "d2s2" => solutions::day2::d2s2(),
+        "d3s1" => solutions::day3::d3s1(),
+        "d3s2" => solutions::day3::d3s2(),
+        _ => {println!("Invalid argument.")},
     }
 }
